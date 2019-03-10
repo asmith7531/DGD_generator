524af93651d8aace701f59c397bb5e03d9024e08
Austin Smith <austinsmith7531@gmail.com>
2019-03-10 19:50:37 -0400

    Basic form structure

---
+++ DGD Generator/script/javascript.js
@@ -0,0 +1,67 @@
+$(document).ready (function() {
+//defines global variables and sets them equal to user input
+  var po=$(".po")
+  console.log(po);
+
+  var address1=$(".address1")
+  console.log(address1)
+
+  var address2=$(".address2")
+  console.log(address2)
+
+  var city=$(".city")
+  console.log(city)
+
+  var region=$(".region")
+  console.log(region)
+
+  var country=$(".country")
+  console.log(country)
+
+  var date=$(".date")
+  console.log(date)
+
+  var batch=$(".batch")
+  console.log(batch)
+
+  var bottles=$(".bottles")
+  console.log(bottles)
+
+  var hazmat=$(".hazmat")
+  console.log(hazmat)
+
+  $(".submitBtn").click(function submit() {
+
+    po=$(".po")
+    console.log(po);
+
+    address1=$(".address1")
+    console.log(address1)
+
+    address2=$(".address2")
+    console.log(address2)
+
+    city=$(".city")
+    console.log(city)
+
+    region=$(".region")
+    console.log(region)
+
+    country=$(".country")
+    console.log(country)
+
+    date=$(".date")
+    console.log(date)
+
+    batch=$(".batch")
+    console.log(batch)
+
+    bottles=$(".bottles")
+    console.log(bottles)
+
+    hazmat=$(".hazmat")
+    console.log(hazmat)
+
+
+  })
+})
